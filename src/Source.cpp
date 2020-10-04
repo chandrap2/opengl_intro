9da7c269f6b4cf1402bbcea5fa685bf9b4858837
chandrap <chandrap31415@gmail.com>
2020-10-03 17:08:21 -0700

    read vertex color attributes

--- src/Source.cpp
+++ src/Source.cpp
@@ -4,6 +4,88 @@
 
 using namespace std;
 
+const char* vertShader =
+    "#version 430\r\n"
+    ""
+    "in layout(location=0) vec2 position;"
+    "in layout(location=1) vec3 inColor;"
+    ""
+    "out vec3 outColor;"
+    ""
+    "void main()"
+    "{"
+    "   gl_Position = vec4(position, 0.0, 1.0);"
+    "   outColor = inColor;"
+    "}";
+
+const char* fragShader =
+    "#version 430\r\n"
+    ""
+    "in vec3 outColor;"
+    ""
+    "out vec4 color;"
+    ""
+    "void main()"
+    "{"
+    "   color = vec4(outColor, 0.0);"
+    "}";
+
+// initializing vertex buffers
+void initBuffers() {
+    GLfloat tri[] = {
+        -0.5f, -0.5f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 0.5f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.2f,
+        0.0f, 0.0f, 1.0f,
+        -0.4f, 0.5f,
+        0.0f, 1.0f, 0.0f,
+        -0.3f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+    };
+
+    GLushort ind[] = { // indices of vertices
+        0, 1, 2,
+        0, 3, 4
+    };
+
+    GLuint bufferID;
+    glGenBuffers(1, &bufferID); // creating a buffer, storing uid inside bufferID
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID); // binding uid to GL_ARRAY_BUFFER
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tri), tri, GL_STATIC_DRAW); // initializing buffer with data
+    glEnableVertexAttribArray(0); // vertex position
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+    glEnableVertexAttribArray(1); // vertex color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+
+    GLuint indBufferID;
+    glGenBuffers(1, &indBufferID); // buffer of vertex indices
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ind), ind, GL_STATIC_DRAW);
+}
+
+// initializing and linking shaders
+void initShaders() {
+    GLuint vertShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char* codeAdapter[1];
+    codeAdapter[0] = vertShader;
+    glShaderSource(vertShaderID, 1, codeAdapter, 0);
+    codeAdapter[0] = fragShader;
+    glShaderSource(fragShaderID, 1, codeAdapter, 0);
+
+    glCompileShader(vertShaderID);
+    glCompileShader(fragShaderID);
+
+    GLuint programID = glCreateProgram();
+    glAttachShader(programID, vertShaderID);
+    glAttachShader(programID, fragShaderID);
+    glLinkProgram(programID);
+    glUseProgram(programID);
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -25,48 +107,17 @@ int main(void)
 
     if (glewInit() != GLEW_OK)
         cout << "glew error";
-
     cout << glGetString(GL_VERSION) << endl;
+
+    initBuffers();
+    initShaders();
     
-    GLfloat tri[] = {
-        -0.5f, -0.5f,
-        1.0f, 0.0f, 0.0f,
-        0.0f, 0.5f,
-        0.0f, 1.0f, 0.0f,
-        0.5f, -0.5f,
-        0.0f, 0.0f, 1.0f,
-        -0.4f, 0.5f,
-        0.0f, 1.0f, 0.0f,
-        -0.3f, 0.0f,
-        0.0f, 0.0f, 1.0f,
-    };
-
-    GLushort ind[] = { // indices of vertices
-        0, 1, 2,
-        0, 3, 4
-    };
-
-    GLuint bufferID;
-    glGenBuffers(1, &bufferID); // creating a buffer, storing uid inside bufferID
-    glBindBuffer(GL_ARRAY_BUFFER, bufferID); // binding uid to GL_ARRAY_BUFFER
-    glBufferData(GL_ARRAY_BUFFER, sizeof(tri), tri, GL_STATIC_DRAW); // initializing buffer with data
-    glEnableVertexAttribArray(0); // vertex position
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
-    glEnableVertexAttribArray(1); // vertex color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char *)(sizeof(float) * 2));
-
-    GLuint indBufferID;
-    glGenBuffers(1, &indBufferID); // buffer of vertex indices
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBufferID);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ind), ind, GL_STATIC_DRAW);
-
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
         /* Swap front and back buffers */
